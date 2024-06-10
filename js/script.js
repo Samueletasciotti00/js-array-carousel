ef4fa73a7f0883fdbf50bb232ee10a4030713be0
Samuele <samueletasciotti@icloud.com>
2024-06-10 16:25:06 +0200

    problems

--- js/script.js
+++ js/script.js
@@ -7,3 +7,17 @@ let immages =[
     '05.webp',
 ]
 //Creare un ciclo che permetta di cambiare immagini dinamicamente;
+
+//Variabile che indica l'immagine specifica nell'array;
+
+// Selezione del pulsante per scorrere giù;
+let down = document.querySelector('.bottom');
+
+down.addEventListener('click',
+    function(){
+        
+    //Verica posizionamento array
+    if ()
+    }
+);
+
