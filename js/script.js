e810e5019197e965b19798b84921078be821cb5b
Samuele <samueletasciotti@icloud.com>
2024-06-10 15:11:01 +0200

    feat: created array

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//Creare un array contentente le immagini;
+let immages =[
+    '01.webp',
+    '02.webp',
+    '03.webp',
+    '04.webp',
+    '05.webp',
+]
+//Creare un ciclo che permetta di cambiare immagini dinamicamente;
